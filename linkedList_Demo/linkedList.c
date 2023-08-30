6e83d9dbc8b4dd572bebf2b43832500cbeb994e8
Kyleh2420 <kyleh2420@gmail.com>
2023-08-30 15:38:29 -0400

    Documented insert function

--- linkedList_Demo/linkedList.c
+++ linkedList_Demo/linkedList.c
@@ -17,6 +17,9 @@ struct node* insert(struct node* list, int value)
 	//We will search the linked list for the proper place to put value, then insert it.
 	//When found, note the pointer. This pointer becomes the inserted values next. 
 	//Will return the head of the entire list
+	
+
+	//Create the variables previous, current, and toInsert
 	struct node* previous;
 	previous = malloc(sizeof(struct node));
 	previous = list;
@@ -29,20 +32,23 @@ struct node* insert(struct node* list, int value)
 	toInsert = malloc(sizeof(struct node));
 	toInsert->data = value;
 
+	//While there still exists a list, follow until the first instance where the number is larger than the given value
 	while (current->next != NULL) {
-		printf("Current value: %d \n", current-> data);
+	//	printf("Current value: %d \n", current-> data);
 		if (current->data > value) {
-			//If these two are the same, we are at the very beginning of the list
+			//If these two are the same, we are at the very beginning of the list. Change the pointers accordingly
 			if (current == previous) {
 				list = toInsert;
 				toInsert->next = current;
 				previous = toInsert;
 			} else {
+				//Otherwise, we can operate as standard
 				previous->next = toInsert;
 				toInsert->next = current;
 			}
 			return list;
 		}
+		//Move along the linked list
 		previous = current;
 		current = current->next;
 
